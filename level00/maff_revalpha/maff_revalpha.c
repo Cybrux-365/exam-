a489a549ecf215420dc7ef6dbdcfa85f30b85f52
Cybrux-365 <laro1laro34@gmail.com>
2024-07-30 12:47:50 +0100

    42

---
+++ level00/maff_revalpha/maff_revalpha.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+int	main(void)
+{
+	int	i;
+	char	c;
+
+	i = 1;
+	c = 122;
+	write (1, &c, 1);
+	while (i < 26)
+	{
+		if (i%2 != 0)
+		{
+			c = c - 33;
+			write (1, &c, 1);
+		}
+		else
+		{
+			c = c + 31;
+			write (1, &c, 1);
+		}
+		i++;
+	}
+	write (1, "\n", 1);
+	return (0);
+}
